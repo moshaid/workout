042ccc1e8ae5a39cadbc3f625822c276d9160465
moshaid <alabidunmoshood@gmail.com>
2024-10-10 19:16:25 +0100

    card on homepage

--- src/componenets/HomePage.tsx
+++ src/componenets/HomePage.tsx
@@ -1,14 +1,36 @@
-import NavBar from "./NavBar"
-import '../App.css'
-
+import NavBar from "./NavBar";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Text,
+  Link,
+} from "@chakra-ui/react";
+import "../App.css";
 
 const HomePage = () => {
   return (
     <>
       <NavBar />
-      <div className="homepage-background"></div>
+      <div className="homepage-background">
+        <Card position='absolute' bottom='0' left='11%' w="35%">
+          <CardHeader>
+            <Heading size="md"> Work Out to Stay Fit</Heading>
+          </CardHeader>
+          <CardBody py="0">
+            <Text>
+            Here on Fithub, you get to work with fitness trainer remotely and enjoy our inHouse fitness equippment.
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <Link backgroundColor="#28488e" p='3' as='b' color="white" borderRadius='xl'>SIGN UP TO STAY FIT</Link>
+          </CardFooter>
+        </Card>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
